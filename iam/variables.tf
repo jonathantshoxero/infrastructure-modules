d8c1d7fc79ea92f6b62bedcbb58fbc490a260c46
jonathantshoxero <jonathan.ho@xero.com>
2022-11-17 15:57:59 -0800

    Remove IAM Group Name

--- iam/variables.tf
+++ iam/variables.tf
@@ -1,8 +1,3 @@
-variable "iam_group_name" {
-  description = "Name of IAM Group"
-  type        = string
-}
-
 variable "env" {
   description = "Terragrunt environment at hand"
   type        = string
